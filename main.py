95c6d5b04b0b58f3aaea60d00cbbb0349acbdb78
Simon-Németh Sámuel <>
2021-04-13 12:27:28 +0000

    Copy Paste from guide

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
 import discord
+import os
 
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(os.getenv
+('ODMxNTAyOTY1MDQyMTg0Mjcy.YHWLew.ZntLxuwlk2KtC81aHu5fd4bDahQ'))
