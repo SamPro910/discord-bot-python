e02d331cfd26eda621abc196a80863be57612520
Simon-Németh Sámuel <>
2021-04-13 12:46:30 +0000

    secure stuff

--- main.py
+++ main.py
@@ -15,5 +15,4 @@ async def on_message(message):
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
-client.run(os.getenv
-('ODMxNTAyOTY1MDQyMTg0Mjcy.YHWLew.ZntLxuwlk2KtC81aHu5fd4bDahQ'))
+client.run(os.getenv('TOKEN'))
