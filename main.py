3512ab7761d775a8c4cf926a5275440c1c8116da
SamPro910 <s.n.samu910@gmail.com>
2021-04-14 13:51:26 +0200

    feleslegtelenítés 2

--- main.py
+++ main.py
@@ -15,13 +15,11 @@ async def on_message(message):
     if message.content.startswith("$bonk"):
         await message.channel.send("https://steamuserimages-a.akamaihd.net/ugc/1618439156949856647/EBFE6D18C67B0599FFA3F25DA20F020E6C9854C8/?imw=637&imh=358&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true")
     if message.content.startswith("$help"):
-        await message.channel.send("Parancsok: \n$hello - Köszönés\n$noob - no u\n$good - kiírja egy jó szerver nevét\n$test3 - kiírja, hogy fent van e a bot\n$credits - kiírja a készítőket\n$bonk - BONK!")
+        await message.channel.send("Parancsok: \n$hello - Köszönés\n$noob - no u\n$test - kiírja, hogy fent van e a bot\n$bonk - BONK!")
     if message.content.startswith('$hello'):
-        await message.channel.send('Helló-Belló!')
+        await message.channel.send('Helló!')
     if message.content.startswith("$noob"):
         await message.channel.send("No u.")
-    if message.content.startswith('$good'):
-        await message.channel.send('BuckaGamin is good')
     if message.content.startswith('$test'):
         await message.channel.send('Működik!')
 
