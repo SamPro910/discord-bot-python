59c341a97dda9dd537e7c95c1b8dfa1eef602f46
SamPro910 <s.n.samu910@gmail.com>
2021-04-14 13:45:37 +0200

    feleslegtelenítés

--- main.py
+++ main.py
@@ -22,13 +22,7 @@ async def on_message(message):
         await message.channel.send("No u.")
     if message.content.startswith('$good'):
         await message.channel.send('BuckaGamin is good')
-    if message.content.startswith('$test3'):
-        await message.channel.send('The Bot is working!')
-    if message.content.startswith("$whois sam"):
-        await message.channel.send("A fő fejlesztője ennek a botnak.")
-    if message.content.startswith("$whois ben"):
-        await message.channel.send("A második fejlesztője a projektnek, ő találta ki.")
-    if message.content.startswith("$credits"):
-        await message.channel.send("Made by Sam and Ben\nTovábbi inforámció a készítőkről: $whois sam/$whois ben")
+    if message.content.startswith('$test'):
+        await message.channel.send('Működik!')
 
 client.run(os.getenv('TOKEN'))
