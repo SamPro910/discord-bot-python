5f1586caf09cea189cdc275839bba74fb4989fff
SamPro910 <79455895+SamPro910@users.noreply.github.com>
2021-04-14 18:20:17 +0200

    +parancs - $random

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import discord
 import os
 import requests
 import json
+import random
 
 client = discord.Client()
+randompar = ["$inspiráció", "$bonk", "$hello", "$noob"]
 def get_quote():
   response = requests.get("https://zenquotes.io/api/random")
   json_data = json.loads(response.text)
@@ -18,13 +20,15 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
+    if message.content.startswith("$random"):
+        await message.channel.send("A random parancs:" + random.choice(randompar))
     if message.content.startswith("$inspiráció"):
         quote = get_quote()
         await message.channel.send(quote)
     if message.content.startswith("$bonk"):
         await message.channel.send("https://steamuserimages-a.akamaihd.net/ugc/1618439156949856647/EBFE6D18C67B0599FFA3F25DA20F020E6C9854C8/?imw=637&imh=358&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true")
     if message.content.startswith("$help"):
-        await message.channel.send("Parancsok: \n$hello - Köszönés\n$noob - no u\n$test - kiírja, hogy fent van e a bot\n$bonk - BONK!\n$inspiráció - egy inspiráló üzenet...")
+        await message.channel.send("Parancsok: \n$hello - Köszönés\n$noob - no u\n$test - kiírja, hogy fent van e a bot\n$bonk - BONK!\n$inspiráció - egy inspiráló üzenet...\n$random - kiír egy random parancsot.")
     if message.content.startswith('$hello'):
         await message.channel.send('Helló!')
     if message.content.startswith("$noob"):
